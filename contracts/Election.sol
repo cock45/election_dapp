834332a16e506ab321af734d2ef330f5785cd32e
cock45 <stardevworld@gmail.com>
2022-05-04 04:09:48 +0900

    add candidate

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,9 +1,23 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Election {
-    string public candidate;
+    struct Candidate {
+        uint256 id;
+        string name;
+        uint256 voteCount;
+    }
+
+    mapping(uint256 => Candidate) public candidates;
+
+    uint256 public candidatesCount;
+
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
 
     constructor() {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
     }
 }
