1870eefbd9da9daa72ee5d286fc0f0245ea3c55a
cock45 <stardevworld@gmail.com>
2022-05-04 09:08:28 +0900

    test code first step

--- truffle-config.js
+++ truffle-config.js
@@ -6,13 +6,9 @@ module.exports = {
 
   networks: {
     development: {
-      provider: () => new HDWalletProvider(MNEMONIC, INFURA_API_KEY),
-      network_id: 3,       // Ropsten's id
-      gas: 5500000,        //  Ropsten has a lower block limit than mainnet
-      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true,     // Skip dry run before migrations? (default: false for public nets )
-      networkCheckTimeout: 1000000000
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
     },
     develop: {
       port: 8545
